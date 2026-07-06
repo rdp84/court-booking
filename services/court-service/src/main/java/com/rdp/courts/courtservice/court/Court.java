6ffaca3670488b3fb7de9557c002d9410b4743cb
Bobby Pearson <pearson.bobby@gmail.com>
2026-07-06 16:10:50 +0100

    Add courts integration test using Testcontainers

---
+++ services/court-service/src/main/java/com/rdp/courts/courtservice/court/Court.java
@@ -0,0 +1,50 @@
+package com.rdp.courts.courtservice.court;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "courts")
+class Court {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private boolean isActive;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    // Required by JPA
+    Court() {
+    }
+
+    Court(final String name, final boolean isActive) {
+        this.name = name;
+        this.isActive = isActive;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
