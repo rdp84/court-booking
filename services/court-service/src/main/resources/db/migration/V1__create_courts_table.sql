312f87d9bf1ff7641b63a316feb67b1f23d71d26
Bobby Pearson <pearson.bobby@gmail.com>
2026-07-07 10:28:39 +0100

    Use Flyway to handle creating database tables

---
+++ services/court-service/src/main/resources/db/migration/V1__create_courts_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE courts (
+       id UUID DEFAULT gen_random_uuid(),
+       name VARCHAR(50) NOT NULL,
+       is_active BOOLEAN NOT NULL DEFAULT true,
+       created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+       CONSTRAINT pk_courts PRIMARY KEY (id)
+);
