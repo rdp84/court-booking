fa1c18fbf7e03ff704de2ed32398b4bf6c66f66d
Bobby Pearson <pearson.bobby@gmail.com>
2026-07-21 17:24:54 +0100

    Add Booking Repository
    Adds the booking repository for the bookings table

---
+++ services/booking-service/src/main/java/com/rdp/bookings/bookingservice/BookingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.rdp.bookings.bookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BookingServiceApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(BookingServiceApplication.class, args);
+    }
+}
